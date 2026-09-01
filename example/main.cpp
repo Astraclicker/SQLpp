ddc2f16fd060eb63cf558f2528cca5b76f53e8fc
Astraclicker <2867314230@qq.com>
2026-09-01 21:27:13 +0800

    修改example

--- example/main.cpp
+++ example/main.cpp
@@ -2,8 +2,8 @@
 
 
 void example_redis() {
-    std::string password = "1108372699a";
-    astra_sql::Redispp redis("139.129.35.66", astra_sql::Redis_DEFAULT_PORT, nullptr, &password, 0);
+    std::string password = "102410";
+    astra_sql::Redispp redis("0.0.0.0", astra_sql::Redis_DEFAULT_PORT, nullptr, &password, 0);
 }
 
 int main() {
